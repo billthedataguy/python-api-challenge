59922f68c1a13aa7028594bfe9f2605c37537303
William Vann <billthelibrarian@gmail.com>
2022-07-27 19:30:55 -0500

    Deleted api keys from repo

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7d4a6572c80773ae279c3bbae2dff68d"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyD8cXjroY7ArdysSV3Ygk6V10en0fOhbh8"
+g_key = ""
